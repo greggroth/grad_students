14cf4a505f519e6448384ece5b0d3d41feeedcf5
Greggory Rothmeier <greggroth@gmail.com>
2012-04-01 16:23:11 -0400

    added ms/phd to professor#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def committee_type(committee)
+    if committee.ms && committee.phd
+      "Both"
+    elsif committee.ms
+      "MS"
+    elsif committee.phd
+      "PhD"
+    else
+      "Bad Record"
+    end
+  end
 end
