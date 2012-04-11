d59d7bb5241547c406967a18975abfefad2e95b6
Greggory Rothmeier <greggroth@gmail.com>
2012-04-11 15:23:00 -0400

    added index action to view the letters for all current students

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,4 +1,16 @@
 class LettersController < ApplicationController
+  
+  def index
+    @department_chair = Professor.where(department_chair: true).first
+    @graduate_advisor = Professor.where(graduate_advisor: true).first
+    @students = Student.current_students
+    if @department_chair.nil? || @graduate_advisor.nil?
+      redirect_to professors_path, notice: "Please ensure a graduate director and department chair have been specified and that the student's stipend has been entered."
+    else
+      render layout: false
+    end
+  end
+  
   def show
     @department_chair = Professor.where(department_chair: true).first
     @graduate_advisor = Professor.where(graduate_advisor: true).first
